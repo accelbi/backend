512a293d8a7c7c373890a85c9d708d0fad56b4d8
Syed Mohammad Abbas <accelbi.info@gmail.com>
2023-11-27 21:30:18 +0530

    Update databaseConfig.js

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -3,15 +3,28 @@ import { MongoClient } from "mongodb";
 const username = encodeURIComponent(process.env.MONGO_USERNAME);
 const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 const MONGODB_URL = process.env.MONGODB_URL || `mongodb+srv://${username}:${password}@cluster0.1t0pt4c.mongodb.net/?retryWrites=true&w=majority`;
-
 let dbEmp , dbMan , dbSuper , dbUser;
+let client; 
+
 const databaseconnect = async () => {
-    const client = new MongoClient(MONGODB_URL);
-    await client.connect();
-    dbEmp = client.db("userEmp");
-    dbMan = client.db("userMan");
-    dbSuper = client.db("super");
-    dbUser = client.db("user");
-}
+    try {
+        if (!client) {
+            client = new MongoClient(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        }
+        if (!client && !client.isConnected()) {
+            await client.connect();
+            console.log("Connected correctly to server");
+        }
+
+        dbEmp = client.db("userEmp");
+        dbMan = client.db("userMan");
+        dbSuper = client.db("super");
+        dbUser = client.db("user");
+    } catch (err) {
+        console.log(err.stack);
+    }
+};
+
+
 
 export { dbEmp , dbMan , dbSuper , dbUser , databaseconnect};
